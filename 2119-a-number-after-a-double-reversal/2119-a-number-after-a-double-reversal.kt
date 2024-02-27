06751da1e6f39dd8fcfddc42d17861a71555e338
Mukesh Yadav <160391417+mukesh-yadav96@users.noreply.github.com>
2024-02-27 23:57:11 +0530

    Time: 125 ms (72.00%), Space: 33.1 MB (68.00%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.kt
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.kt
@@ -1,6 +1,3 @@
 class Solution {
-    fun isSameAfterReversals(num: Int): Boolean {
-        if(num<10) return true
-        return num.toString().reversed().trimStart('0')==num.toString().reversed()
-    }
+    fun isSameAfterReversals(num: Int) = num%10 != 0 || num==0
 }
