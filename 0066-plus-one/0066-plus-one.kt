dfab0646776bed38b1d64c7007210fa0cf0d437d
Mukesh Yadav <160391417+mukesh-yadav96@users.noreply.github.com>
2024-02-29 23:59:39 +0530

    Time: 151 ms (92.62%), Space: 34.4 MB (96.91%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        for (i in digits.size - 1 downTo 0) {
+            digits[i] += 1
+            if (digits[i] <= 9) return digits
+            digits[i] = 0
+        }
+        val arr = IntArray(digits.size + 1)
+        arr[0] = 1
+
+        return arr
+    }
+}
