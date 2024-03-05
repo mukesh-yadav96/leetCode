5871fb2fbdc79010bb8d084d797048d092225ee2
Mukesh Yadav <160391417+mukesh-yadav96@users.noreply.github.com>
2024-03-05 21:37:32 +0530

    Time: 272 ms (62.57%), Space: 44 MB (41.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.kt
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.kt
@@ -1,9 +1,7 @@
 class Solution {
     fun topKFrequent(nums: IntArray, k: Int): IntArray {
-    val hm = HashMap<Int,Int>()
-    nums.forEach {
-        hm[it] = hm.getOrDefault(it,0)+1
-    }
-    return hm.entries.sortedByDescending { it.value }.take(k).map { it.key }.toIntArray()
+    return nums.groupBy { it }.entries
+        .sortedByDescending { it.value.size }
+        .take(k).map { it.key }.toIntArray()
     }
 }
