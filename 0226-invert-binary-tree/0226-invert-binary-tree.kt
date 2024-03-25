bbc79122e584763b43f302051af52517ec999250
Mukesh Yadav <160391417+mukesh-yadav96@users.noreply.github.com>
2024-03-25 23:20:22 +0530

    Time: 147 ms (38.20%), Space: 34 MB (36.53%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.kt
@@ -0,0 +1,18 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? = root?.let {
+        val tmp = it.left
+        it.left = invertTree(it.right)
+        it.right = invertTree(tmp)
+        it
+    }       
+}
