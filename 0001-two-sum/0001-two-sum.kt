679157af955e939dc94594bf3d30b668b7eda407
Mukesh Yadav <160391417+mukesh-yadav96@users.noreply.github.com>
2026-06-28 19:24:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,13 +1,5 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val hm = HashMap<Int, Int>()
-        for(i in nums.indices) {
-            val comp = target - nums[i]
-            if(comp in hm) {
-                return intArrayOf(hm[comp]!!, i)
-            }
-            hm[nums[i]] = i
-        }
-        throw IllegalArgumentException("No two solution")
+        
     }
 }
